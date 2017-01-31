6cb7a06c376f8a746c09aa5e4aa263aef968303f
emisner <emisner@apps.district833.org>
2017-01-30 20:22:25 -0600

    Update ArduinoSpeedControl.ino

--- ArduinoSpeedControl/ArduinoSpeedControl.ino
+++ ArduinoSpeedControl/ArduinoSpeedControl.ino
@@ -1,34 +1,44 @@
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-
-int potpin = A4;  // analog pin used to connect the potentiometer
+// Speed Control
+//Servo myPotServo;  // create servo object to control a servo
+const int potpin = A4;  // analog pin used to connect the potentiometer
 int val;    // variable to read the value from the analog pin
 int sensorValue;
 int oldValue = 1500;
 
+// Button Control
+Servo myServo;
+const int motorPin = 3;
+const int buttonPin = 7;
+int buttonState = 0;
+
 void setup() {
-  myservo.attach(3, 1000, 2000);  // attaches the servo on pin 3 to the servo object
+  // put your setup code here, to run once:
+  myServo.attach(motorPin, 1000, 2000);
+  pinMode(buttonPin, INPUT);
 //  Serial.begin(9600);
-  }
-
-void loop() {
-//  val = analogRead(A4);            // reads the value of the potentiometer (value between 0 and 1023)
-  sensorValue = analogRead(potpin);
-/* Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-*  float voltage = sensorValue * (5.0 / 1023.0);
-*  print out the value you read:
-*  Serial.println(voltage);
-*/
-
-//to do, if pot didn't change then don't write
-  if (sensorValue != oldValue) {
-    sensorValue = (sensorValue + 7 * oldValue) / 8;  // filter the value to avoid jumps if noisy pot
-    val = map(sensorValue, 1023, 0, 1500, 2000);     // scale it to use it with the servo (value between 0 and 180)
-    myservo.writeMicroseconds(val);                  // sets the servo position according to the scaled value
-    oldValue = sensorValue;
-  }
-  delay(15);
-                             
 }
 
+void loop() {
+  // Speed Control
+  sensorValue = analogRead(potpin);
+
+  // Switch Control
+  if (digitalRead(buttonPin) == HIGH)
+    {
+      myServo.writeMicroseconds(1500);
+//      Serial.print("HIGH\n");
+      oldValue = 1500;                  // sets oldValue to its default value (1500=off)
+    }
+    else
+    {
+      if (sensorValue != oldValue) {
+        sensorValue = (sensorValue + 7 * oldValue) / 8;  // filter the value to avoid jumps if noisy pot
+        val = map(sensorValue, 1023, 0, 1500, 2000);     // scale it to use it with the servo (value between 0 and 180)
+        myServo.writeMicroseconds(val);                  // sets the servo position according to the scaled value
+        oldValue = sensorValue;  
+      }      
+      delay(15);
+    }
+}
